90f9a65db92f23260c85ea4967f984388e2d13b7
Saurav Patil <sauravpatil453@gmail.com>
2024-12-24 13:32:00 +0530

    UI Enhanced

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.pixabay.com"],
+    remotePatterns: [
+      {
+        hostname: "cdn.pixabay.com",
+      },
+    ],
   },
 };
 
